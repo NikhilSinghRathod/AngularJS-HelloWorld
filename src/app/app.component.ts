b8695ae7a7c1c32dd05d990f2dc3deb093325525
NikhilSinghRathod <r7445580@gmail.com>
2025-03-07 13:45:05 +0530

    [Nikhil Singh Rathode] Add. To display Hello from BridgeLabz with a user inputted name - Ensure the userName

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,6 +13,7 @@ export class AppComponent {
   imgUrl= "assets/BL_logo_square_jpg.jpg";
   url = "https://www.bridgelabz.com";
   userName: string = "";
+  nameError: string ="";
 
 
   ngOnInit(): void {
@@ -24,4 +25,15 @@ export class AppComponent {
     console.log("Save button is clicked!", $event);
     window.open(this.url, "_blank");
   }
+
+  onInput($event:Event){
+    console.log("Change Event Occurred!", $event);
+    const nameRegex = RegExp('^[A-Z]{1}[a-zA-Z\\s]{2,}$');
+    if(nameRegex.test(this.userName)){
+      this.nameError="";
+      return;
+    }
+    this.nameError = "Name is Incorrect!";
+  }
 }
+
