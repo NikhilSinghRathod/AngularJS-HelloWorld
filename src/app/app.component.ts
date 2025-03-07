6a88b95ff2e6bb639a1facbb9388a6ce68a039c8
NikhilSinghRathod <r7445580@gmail.com>
2025-03-07 13:07:21 +0530

    [Nikhil Singh Rathode] Add. To display Hello from BridgeLabz with a user inputted name - Make sure to import Form Module needed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,27 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'Helloworld';
-  imgUrl="assets/BL_logo_square_jpg.jpg";
-  url = "https://www.bridgelabz.com/"
+  imgUrl= "assets/BL_logo_square_jpg.jpg";
+  url = "https://www.bridgelabz.com";
+  userName: string = "";
 
-  ngOnInit(): void{
-    this.title = "Hello from BridgeLabz. ";
+
+  ngOnInit(): void {
+    this.title = "Hello from BridgeLabz.";
+    
   }
 
-  onClick($event:Event){
+  onClick($event: Event){
     console.log("Save button is clicked!", $event);
-    window.open(this.url,"_blank");
+    window.open(this.url, "_blank");
   }
 }
